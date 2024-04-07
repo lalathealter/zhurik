d7297c048f481271ff880d6d4b1e2cc2877d777d
lalathealter <musin.daniiil@yandex.ru>
2024-04-07 18:03:25 +0500

    улучшил наименование переменных для ясности

--- db_init.py
+++ db_init.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 users_table_name = "users"
-users_table = f""" CREATE TABLE IF NOT EXISTS {users_table_name} (
+users_table_setting = f""" CREATE TABLE IF NOT EXISTS {users_table_name} (
                 id INTEGER PRIMARY KEY,
                 ask_count int NOT NULL CHECK (ask_count >= 0) DEFAULT 1,
                 name TEXT NOT NULL,
@@ -9,7 +9,7 @@ users_table = f""" CREATE TABLE IF NOT EXISTS {users_table_name} (
             ); """
 
 questions_table_name = "questions"
-questions_table = f"""CREATE TABLE IF NOT EXISTS {questions_table_name} (
+questions_table_setting = f"""CREATE TABLE IF NOT EXISTS {questions_table_name} (
                         id INTEGER PRIMARY KEY,
                         ask_count INT NOT NULL CHECK(ask_count >= 0) DEFAULT 0,
                         prompt TEXT NOT NULL,
@@ -17,15 +17,15 @@ questions_table = f"""CREATE TABLE IF NOT EXISTS {questions_table_name} (
                      ); """
 
 table_names = [users_table_name, questions_table_name]
-tables = [users_table, questions_table]
+table_statements = [users_table_setting, questions_table_setting]
 
 
 def main():
     connection = sqlite3.connect('zhurik.db')
     cursor = connection.cursor()
 
-    for table in tables:
-        cursor.execute(table)
+    for table_statement in table_statements:
+        cursor.execute(table_statement)
 
     print("База данных успешно создана. Благодарение Богу!")
     connection.close()
