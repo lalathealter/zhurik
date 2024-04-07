6ffac69ac933aa3d3668f8799f87dbe6f573adf6
lalathealter <musin.daniiil@yandex.ru>
2024-04-07 19:25:50 +0500

    написал более-менее полноценный скрипт для выгрузки статистики о запрошенных вопросах - теперь в формате csv

--- db_stat.py
+++ db_stat.py
@@ -1,10 +1,18 @@
+import csv, datetime
 from db_init import questions_table_name, db_connect
 
 
 get_statistics_statement = f"""
-    SELECT parent_prompt, prompt, ask_count
+    SELECT prompt, parent_prompt, ask_count
     FROM {questions_table_name}
 """
+
+
+def generate_statfile_name():
+    time_format = "%d %b %Y %H-%M-%S"
+    now_time = datetime.datetime.now()
+    time_string = now_time.strftime(time_format)
+    return f"questions_table_statistics - {time_string}"
 
 
 def main():
@@ -12,13 +20,26 @@ def main():
     cursor = connection.cursor()
 
     cursor.execute(get_statistics_statement)
-
     rows = cursor.fetchall()
 
-    print("=== Выгрузка из базы данных по теме: Статистика заданных вопросов ===")
-    for row in rows:
-        print(row)
-    print("=== Конец выгрузки; Благодарение Богу! ===")
+    filepath = f"./stats/{generate_statfile_name()}.csv"
+    with open(filepath, 'w', newline='') as csvfile:
+        print("=== Выгрузка из базы данных по теме: Статистика заданных вопросов ===")
+
+        spamwriter = csv.writer(
+            csvfile,
+            delimiter='|',
+            quotechar='|',
+            quoting=csv.QUOTE_MINIMAL
+        )
+        spamwriter.writerow(['Вопрос', 'Тема', 'Запросы'])
+        for row in rows:
+            print(row)
+            spamwriter.writerow(row)
+
+        print("=== Конец выгрузки; Благодарение Богу! ===")
+
+    connection.close()
 
 
 if __name__ == "__main__":
